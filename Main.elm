83e98a771bce65aefe1b6eb5ec6bfca27315cb62
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-10-17 23:04:14 -0700

    Remove BlockContent wrapper type

--- Main.elm
+++ Main.elm
@@ -11,10 +11,6 @@ import Debug
 type Document = [Block]
 
 data Block =
-  WithSelection BlockContent |
-  NoSelection BlockContent
-
-data BlockContent =
   Heading Int Span | -- level, content
   Paragraph Span | -- content
   CodeBlock (Maybe String) String -- language, content
@@ -29,10 +25,6 @@ toHtml d = node "div" [] <| map blockToHtml d
 
 blockToHtml : Block -> Html
 blockToHtml b = case b of
-  WithSelection c -> node "div" [ class "selected" ] [ blockContentToHtml c ]
-  NoSelection c -> blockContentToHtml c
-
-blockContentToHtml b = case b of
   Heading 1 s -> node "h1" [] [ spanToHtml s ]
   Heading _ s -> node "h2" [] [ spanToHtml s ]
   Paragraph s -> node "p" [] [ spanToHtml s ]
@@ -45,9 +37,9 @@ spanToHtml span = case span of
 ---- App
 
 exampleDoc = [
-  WithSelection <| Heading 1 (Plain "Welcome to Elm"),
-  NoSelection <| Paragraph (Plain "A functional reactive language for interactive applications"),
-  NoSelection <| CodeBlock (Just "elm") "main = asText \"Hello World\""
+  Heading 1 (Plain "Welcome to Elm"),
+  Paragraph (Plain "A functional reactive language for interactive applications"),
+  CodeBlock (Just "elm") "main = asText \"Hello World\""
   ]
 
 --main = toElement 800 600 <| toHtml exampleDoc
