3a247702b0a27a0ee7e964944fae3b143dcb7bf1
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-10-26 20:56:11 -0700

    Remove Inbox child from example data since there is a real inbox now

--- SampleData.elm
+++ SampleData.elm
@@ -2,20 +2,19 @@ module SampleData where
 
 import Outline.Entry as Entry
 
-template = Entry.Entry { text="Tasks (LOADING...)", description="", inbox=[], children=[
-    Entry.Entry { text="Inbox", description="", inbox=["sdfs","sdfsd"], children=[] },
-    Entry.Entry { text="By time (deadline)", description="", inbox=[], children=[
-      Entry.Entry { text="daily", description="", inbox=[], children=[] },
-      Entry.Entry { text="weekly", description="", inbox=[], children=[] },
-      Entry.Entry { text="waiting on", description="", inbox=[], children=[] },
-      Entry.Entry { text="monthly", description="", inbox=[], children=[] },
-      Entry.Entry { text="yearly, etc.", description="", inbox=[], children=[] }
-    ] },
-    Entry.Entry { text="Habits", description="", inbox=[], children=[
-      Entry.Entry { text="daily", description="", inbox=[], children=[] },
-      Entry.Entry { text="weekly", description="", inbox=[], children=[] },
-      Entry.Entry { text="monthly", description="", inbox=[], children=[] }
-    ] },
-    Entry.Entry { text="By priorty", description="", inbox=[], children=[] },
-    Entry.Entry { text="By project", description="", inbox=[], children=[] }
-    ] }
+template = Entry.Entry { text="Tasks (LOADING...)", description="", inbox=["sdfs","sdfsd"], children=[
+  Entry.Entry { text="By time (deadline)", description="", inbox=[], children=[
+    Entry.Entry { text="daily", description="", inbox=[], children=[] },
+    Entry.Entry { text="weekly", description="", inbox=[], children=[] },
+    Entry.Entry { text="waiting on", description="", inbox=[], children=[] },
+    Entry.Entry { text="monthly", description="", inbox=[], children=[] },
+    Entry.Entry { text="yearly, etc.", description="", inbox=[], children=[] }
+  ] },
+  Entry.Entry { text="Habits", description="", inbox=[], children=[
+    Entry.Entry { text="daily", description="", inbox=[], children=[] },
+    Entry.Entry { text="weekly", description="", inbox=[], children=[] },
+    Entry.Entry { text="monthly", description="", inbox=[], children=[] }
+  ] },
+  Entry.Entry { text="By priorty", description="", inbox=[], children=[] },
+  Entry.Entry { text="By project", description="", inbox=[], children=[] }
+  ] }
--- Test/MainTest.elm
+++ Test/MainTest.elm
@@ -12,18 +12,18 @@ test1 = test "first-use scenario" <|
   foldl App.step (App.Model (entry "" "" [] []) (InText 0))
     [ Key (Character "Tasks")
     , Key (Command "a") -- add
-    , Key (Character "Inbox")
-    , Key (Enter)
     , Key (Character "By time")
+    , Key (Enter)
+    , Key (Character "Habits")
     , Key (Command "p") -- promote
     , Key (Command "p")
     ]
   `assertEqual`
   App.Model
     (entry "Tasks" "" []
-      [ entry "Inbox" "" [] []
-      , entry "By time" "" [] []
+      [ entry "By time" "" [] []
+      , entry "Habits" "" [] []
       ])
-    (InChild (0,InText 7))
+    (InChild (0,InText 6))
 
 suite = Suite "Integration tests" [test1]
