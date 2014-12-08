a93f55d7fc770929a70e2d5c2dea4fcece75a9c7
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-12-07 20:05:53 -0800

    Correctly render newlines in strings

--- App/Render/String.elm
+++ App/Render/String.elm
@@ -5,10 +5,22 @@ import Graphics.Element (..)
 import Html (..)
 import Html.Attributes (..)
 import String
+import List
+
+line : String -> Html
+line string = node "p" [] [ text string ]
+
+lines : String -> List Html
+lines stringWithNewlines = stringWithNewlines
+  |> String.split "\n"
+  |> List.map line
 
 render : Core.String.Zipper -> Element
-render (value,cursor) = node "span" [] [
-  text <| String.left cursor value,
-  node "span" [ class "cursor" ] [ text "^" ],
-  text <| String.dropLeft cursor value ]
+render (value,cursor) = node "span"
+  [ style [ ("white-space", "pre-wrap")]
+  ]
+  [ text <| String.left cursor value
+  , node "span" [ class "cursor" ] [ text "^" ]
+  , text <| String.dropLeft cursor value
+  ]
   |> toElement 100 100
