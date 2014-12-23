61dbbce82621111dc39ef0d16b3436d9be0362f5
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-12-23 17:18:10 -0500

    Allow left/right navigation between RichText.Blocks

--- Outline/RichText/Actions.elm
+++ Outline/RichText/Actions.elm
@@ -12,16 +12,19 @@ import Core.Array
 
 type alias Result = ActionResult Value Zipper
 
+doArrayMaybe : (Zipper -> Maybe Zipper) -> Zipper -> Result
+doArrayMaybe fn z = case fn z of
+  Just z' -> Update z'
+  Nothing -> NoChange
+
 doBlock : (Block.Zipper -> Block.Result) -> Zipper -> Result
 doBlock fn z = case fn (Core.Array.active z) of
   Block.Update z' -> Update <| Core.Array.replaceActive z' z
   Block.Split left z' right -> Update <| Core.Array.mergeActive left z' right z
-  Block.Join v' -> case Core.Array.joinActive Block.mergeZipper v' z of
-    Nothing -> NoChange
-    Just z'' -> Update z''
+  Block.Join v' -> doArrayMaybe (Core.Array.joinActive Block.mergeZipper v') z
+  Block.EnterPrev -> doArrayMaybe (Core.Array.goPrev Block.toValue Block.endZipper) z
+  Block.EnterNext -> doArrayMaybe (Core.Array.goNext Block.toValue Block.startZipper) z
   Block.NoChange -> NoChange
-  Block.EnterPrev -> NoChange
-  Block.EnterNext -> NoChange
 
 -- TODO: on a split, or update, merge adjacent spans with the same type
 doSpan : (Span.Zipper -> Span.Result) -> Zipper -> Result
